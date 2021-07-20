b4b60912dc924e005ddbd60147b69d5c03befb00
Jimeneab <abraham.tal.ji@gmail.com>
2021-07-20 15:36:29 -0500

    checkbox control

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import RigthSideBar from './components/rigthSideBar';
+import CheckBoxForm from './components/checkboxform';
 import './App.css';
 import React from 'react'
 
 function App() {
   return (
     <div className="App" id="outer-container">
+      <RigthSideBar> </RigthSideBar>
       <div id="page-wrap">
         <h1>Esta es la pagina pricipal.</h1>
         <h2>Este es el subtitulo.</h2>
-        <RigthSideBar></RigthSideBar>
+        <CheckBoxForm></CheckBoxForm>
       </div>
     </div>
   );
---
+++ src/components/checkboxform.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import {pantryData} from "../data/data";
+
+export default function CheckBoxForm(){
+  let [checked,setChecked] = useState(new Array(pantryData.length).fill())
+  let [totalCheck, setTotalCheck] = useState(0)
+
+  function submit(e){}
+
+  function handleOnchange(e){
+    console.log(e.target.name)
+    console.log(e.target.value)
+    checked[e.target.name] = e.target.value
+    setChecked(!checked)
+    console.log(checked)
+  }
+
+  // const handleOnChange = (position) => {
+  //   let updatedCheckedState = checked.map((item, index)=>{
+  //     index==position? !item :item
+  //   });
+  //   setChecked(updatedCheckedState)
+  // }
+
+    return(
+    <div id="checkBoxForm">
+     <form>
+      <div className='form-group'>
+       <label>Campo de texto!!</label>
+       <input type='text' className='form-control' placeholder='Escribe algo...'></input>
+       <small>Gracias por tu preferencia!!!</small>
+      </div>
+      <div className='form-group'>
+        <label>Password</label>
+        <input type='password' className='form-control'></input>
+      </div>
+      <div className='form-check'>
+        {pantryData.map((element,index) => {
+         return (
+           <div key={index}>
+             <label><input type='checkbox' checked={checked[index]} onChange={handleOnchange} name={index}></input>{element.pantryItem.name}</label>
+           </div>
+         )
+        })}
+      </div>
+     </form>
+    </div>
+    );
+}
